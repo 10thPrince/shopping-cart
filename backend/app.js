a615c62f4eca7662d972328ba516179050544c1c
10thPrince <ntwariprince5050@gmail.com>
2024-12-24 22:02:52 +0200

    added frontent files

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,21 @@
 import express, { json } from "express";
-import { PORT,  } from "./config.js";
+import { PORT  } from "./config.js";
 import { connectDB } from "./config/db.js";
 import productRouter from "./routes/product.route.js";
+import cors from "cors"
 
 
 const app = express();
+
+//app.use(cors());
+
+app.use(
+    cors({
+        origin: `http://localhost:${PORT}`,
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+)
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import CreateProduct from './pages/CreateProduct'
+import EditProduct from './pages/EditProduct'
+import DeleteProduct from './pages/DeleteProduct'
+import ShowProduct from './pages/ShowProduct'
+import { Box } from '@chakra-ui/react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products/create' element={<CreateProduct />} />
+        <Route path='/products/edit/:id' element={<EditProduct />} />
+        <Route path='/products/delete/:id' element={<DeleteProduct />} />
+        <Route path='/products/details/:id' element={<ShowProduct />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default App
